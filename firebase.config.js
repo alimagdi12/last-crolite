a1b52f1dac274d7a526d2f81747d927608b99588
alimagdi25 <alimagdi12367@gmail.com>
2024-07-20 02:10:36 +0300

    search commit

--- firebase.config.js
+++ firebase.config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-const { initializeApp } = require('firebase/app');
-const { getAuth } = require('firebase/auth');
-const { getStorage } = require('firebase/storage');
+const { initializeApp } = require("firebase/app");
+const { getAuth } = require("firebase/auth");
+const { getStorage } = require("firebase/storage");
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_key,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
