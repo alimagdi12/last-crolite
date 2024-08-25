91078e6fc03c3a75d0857fc3f681458cb0be057b
alimagdi25 <alimagdi12367@gmail.com>
2024-08-26 02:20:12 +0300

    solving the is-auth middleware redirection problem

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,14 +1,21 @@
 const jwt = require("jsonwebtoken");
 
+module.exports = async (req, res, next) => {
+  const token = req.cookies.token;
 
-module.exports =async (req, res, next) => {
-    
-    const token = await req.cookies.token;
+  if (!token) {
+    return res.redirect("/login");
+  }
+
+  try {
     const decodedToken = await jwt.verify(token, "your_secret_key");
-    const email = decodedToken.email;
 
-    if (!token || !decodedToken  || email !== 'alimagdi12367@gmail.com') {
-        return res.redirect('/login');
+    if (decodedToken.email !== "alimagdi12367@gmail.com") {
+      return res.redirect("/login");
     }
+
     next();
-}
+  } catch (err) {
+    return res.redirect("/login");
+  }
+};
